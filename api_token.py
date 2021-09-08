28f68fa8a01ae64118dabeb8974c2b5182471b36
Gus de Souza <gustavo.souza@avidbots.com>
2021-09-08 18:37:00 -0400

    Refactor packagecloud

---
+++ api_token.py
@@ -0,0 +1,65 @@
+import os
+import json
+import logging
+
+
+def read_token_from_env():
+    """Reads the token from environment variable PACKAGECLOUD_API_KEY
+       
+    Returns
+    -------
+    string
+        The API token string or a blank string if the variable does not exist
+    """
+
+    return os.environ.get('PACKAGECLOUD_API_KEY', '')
+
+
+def read_token_from_file(config_file):
+    """Reads the token from the file ~/.packagecloud
+       
+    Parameters
+    ----------
+    config_file: string
+        The packagecloud configuration file path
+
+    Returns
+    -------
+    string
+        The API token string or a blank string if the variable does not exist
+    """
+
+    if not os.path.isfile(config_file):
+        return ''
+    
+    with open(config_file, "r") as file:
+        contents = file.read()
+    
+    try:
+        api_token = json.loads(contents).get('token', '')
+    except json.decoder.JSONDecodeError:
+        logging.warning(f"Could not decode JSON in {config_file}")
+        api_token = ''
+    finally:
+        return api_token
+
+
+def load_token():
+    """Load packagecloud token
+    
+    Try to use env var PACKAGECLOUD_API_KEY first (for jenkins jobs)
+    If it is not set then try to read it from ~/.packagecloud 
+    
+    Returns
+    -------
+    string
+        The API token string or a blank string if the variable does not exist
+    """
+    home_dir = os.path.expanduser("~")
+    cfg_file = os.path.join(home_dir, ".packagecloud")
+
+    return read_token_from_env() or read_token_from_file(cfg_file)
+
+
+
+
